fe9f6e13eb082af622f813fdda8f07ca5873f842
Mehdi boughambouz <mehdi.boughambouz@talan.com>
2022-06-03 04:03:58 +0200

    MyFirstBookStore

---
+++ BookStore.Core/DbClient.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Core
+{
+    public class DbClient : IDbClient
+    {
+        private readonly IMongoCollection<Book> _books;
+        public DbClient(IOptions<BookStoreDbConfig> bookStoreDbConfig)
+        {
+            var client = new MongoClient(bookStoreDbConfig.Value.Connection_String);
+            var database = client.GetDatabase(bookStoreDbConfig.Value.Database_Name);
+            _books = database.GetCollection<Book>(bookStoreDbConfig.Value.Books_Collection_Name);
+        }
+        public IMongoCollection<Book> GetBooksCollection() => _books;
+        
+    }
+}
